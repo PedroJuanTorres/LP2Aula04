9f7a4bf842f2bcd83d5ee47ebf8455944e3cc3d0
Pedro Torres <pedrojuantorres.info@gmail.com>
2021-10-19 12:03:48 +0100

    Solve Ex02

---
+++ Exercicio2/Assets/Scripts/Ex02.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ex02 : MonoBehaviour
+{
+    [SerializeField]private float secHello = 4;
+    [SerializeField]private float secUp = 1;
+    [SerializeField]private float setStopHello = 30;
+
+    private Coroutine printHello;
+
+
+    private IEnumerator PrintHello(float secs)
+    {
+        WaitForSeconds wfs = new WaitForSeconds(secs);
+        while (true)
+        {
+            Debug.Log("Ônibus");
+            yield return wfs;
+        }
+    }
+
+    private IEnumerator CountUp(float secs, float secsStopHello)
+    {
+        int count = 0;
+        WaitForSeconds wfs = new WaitForSeconds(secs);
+
+        while (true)
+        {
+            Debug.Log($"{count}");
+            count ++;
+
+            if (count == secsStopHello)
+            {
+                StopCoroutine(printHello);
+            }
+
+            yield return wfs;
+        }
+    }
+
+    private void Start()
+    {
+        printHello = StartCoroutine(PrintHello(secHello));
+        StartCoroutine(CountUp(secUp,setStopHello));
+    }
+
+    
+    private void Update()
+    {
+        
+    }
+}
